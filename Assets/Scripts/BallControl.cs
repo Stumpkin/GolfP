710cb8e043da1d8f1ad2e219886a1d1f4cbd0e37
Stumpkin <banksjalen0@gmail.com>
2020-04-19 16:03:12 -0700

    Inital commit

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.Mouse0))
+        {
+            Vector3 direction = new Vector3(0, 0, 5);
+            rb.AddForce(direction);
+            Debug.Log("LOL");
+        }
+        
+    }
+}
