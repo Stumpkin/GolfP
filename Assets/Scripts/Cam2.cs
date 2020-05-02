e534a7284f984b1a495e6bd80b66f4d16c7e1470
Stumpkin <banksjalen0@gmail.com>
2020-05-01 23:06:29 -0700

    Camera Fixed
    Change: Finally the camera can rotate and follow the player. "CamControl.cs" was sacraficed for "Cam2.cs"
    
    Change: Added more box coliders for the hole can collect ballse securly

---
+++ Assets/Scripts/Cam2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam2 : MonoBehaviour
+{
+    public Transform Target;
+    public BallControl ball;
+    [SerializeField] Vector3 offSet;
+    [SerializeField] Vector3 rot;
+    [SerializeField] Vector3 temp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offSet = new Vector3(Target.position.x - .2f, Target.position.y + .5f, Target.position.z + 3);
+        //temp = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Target.position + offSet;
+
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            offSet = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * 8f, Vector3.up) * offSet;
+            transform.LookAt(Target.transform);
+        }
+
+           
+    }
+}
---
+++ Assets/Scripts/Hoke.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hoke : MonoBehaviour
+{
+    int timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Something hit me XI");
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        Debug.Log("Something entered me OwO");
+        if (timer > 500)
+        {
+            Destroy(collider.gameObject);
+            timer = 0;
+        }
+        timer++;
+
+    }
+}
